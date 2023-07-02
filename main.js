d815cca429905754b7a902c398db1efac66bdbdf
jisooround <9utesleben@gmail.com>
2023-07-02 15:31:58 +0900

    design: style 수정

--- main.js
+++ main.js
@@ -1,22 +1,7 @@
 const container = document.getElementById("container");
-// const items = container.getElementsByClassName("item");
 
 for (let i = 0; i < 12; i++) {
   const itemBox = document.createElement("div");
-  // 텍스트는 마지막에 지우기
-  // itemBox.textContent = `${i + 1}`;
-  itemBox.className = `item box${i + 1}`;
-  // itemBox.style.borderLeft = "4px solid black";
+  itemBox.className = `box${i + 1}`;
   container.appendChild(itemBox);
 }
-
-// for (let j = 0; j < 12; j++) {
-//   const item = document.getElementsByClassName(`item`);
-//   const thisBoxStyle = getComputedStyle(item[j]).gridColumnStart;
-//   console.log(thisBoxStyle);
-//   if (thisBoxStyle === "1") {
-//     console.log("hi");
-//     thisBoxStyle.style.borderLeft = "none";
-//   }
-//   // console.log(itemBox.style.gridColumnEnd);
-// }
