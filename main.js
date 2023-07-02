06f7e9200399782f16419e3492fce63aeb47becd
jisooround <9utesleben@gmail.com>
2023-07-02 15:37:43 +0900

    fix: 불필요한 div 삭제

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const container = document.getElementById("container");
 
-for (let i = 0; i < 12; i++) {
+for (let i = 0; i < 10; i++) {
   const itemBox = document.createElement("div");
   itemBox.className = `box${i + 1}`;
   container.appendChild(itemBox);
